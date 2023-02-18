298e36faa5a64fcac739539b5ffc30554859175a
maomao <2892798998@qq.com>
2023-02-18 19:17:42 +0800

    init

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
+	"runtime"
+)
 
 func main() {
+	// game
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	handlerFn := func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Print(request.Header)
+		writer.Write([]byte("hello"))
+	}
+	http.HandleFunc("/", handlerFn)
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println("hello, game")
 }
